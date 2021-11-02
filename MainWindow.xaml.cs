859f7a9f98b96feae858dc882419c1ce8e1c678e
AnatolFil <tolyanfilatov@mail.ru>
2021-11-02 21:20:32 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace weatherForecast
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async Task getWeatherForecast()
+        {
+            var responseString = await client.GetStringAsync("api.openweathermap.org/data/2.5/forecast?id=539839&appid=a1844320d6a512c4c61a813ca116763d");
+
+        }
+
+    }
+}
